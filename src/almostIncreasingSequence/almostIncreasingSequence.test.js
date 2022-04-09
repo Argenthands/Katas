1b4c8b70462303560c518c5e9bf7be64dafc9627
AlejandroByMacBookPro18 <alejandro.david.benolol@gmail.com>
2022-04-09 21:32:12 +0300

    arreglados y agregados test al almostIncreasingSequence

--- src/almostIncreasingSequence/almostIncreasingSequence.test.js
+++ src/almostIncreasingSequence/almostIncreasingSequence.test.js
@@ -1,20 +1,4 @@
 const almostIncreasingSequence = require('./almostIncreasingSequence')
-const test_5 = [1, 1, 2, 3, 4, 4] // false
-const test_6 = [1, 4, 10, 4, 2] // false
-const test_7 = [10, 1, 2, 3, 4, 5] // true
-const test_8 = [1, 1, 1, 2, 3] // false
-const test_9 = [0, -2, 5, 6] // true
-const test_10 = [1, 2, 3, 4, 5, 3, 5, 6] // false
-const test_11 = [40, 50, 60, 10, 20, 30] // false
-const test_12 = [1, 1] // true
-const test_13 = [1, 2, 5, 3, 5] // true
-const test_14 = [1, 2, 5, 5, 5] // false
-const test_15 = [10, 1, 2, 3, 4, 5, 6, 1] // false
-const test_16 = [1, 2, 3, 4, 3, 6] // true
-const test_17 = [1, 2, 3, 4, 99, 5, 6] // true
-const test_18 = [123, -17, -5, 1, 2, 3, 12, 43, 45] // true
-const test_19 = [3, 5, 67, 98, 3] // trues
-
 describe('Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.', ()=>{
 
     test('should return false if receive the array [1, 3, 2, 1]', ()=>{
@@ -37,39 +21,78 @@ describe('Given a sequence of integers as an array, determine whether it is poss
         expect(almostIncreasingSequence(input)).toBe(false)
     }),
 
-    test('should return false if receive the array ${test_5}', ()=>{
-        expect(almostIncreasingSequence(test_5)).toBe(false)
+    test('should return false if receive the array [1, 1, 2, 3, 4, 4]', ()=>{
+        const input = [1, 1, 2, 3, 4, 4] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
+    }),
+    
+    test('should return false if receive the array [1, 4, 10, 4, 2]', ()=>{
+        const input = [1, 4, 10, 4, 2] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
     }),
 
-    test('should return false if receive the array ${test_6}', ()=>{
-        expect(almostIncreasingSequence(test_6)).toBe(false)
+    test('should return true if receive the array [10, 1, 2, 3, 4, 5]', ()=>{
+        const input = [10, 1, 2, 3, 4, 5] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
+    }),
+    
+    test('should return false if receive the array [1, 1, 1, 2, 3]', ()=>{
+        const input = [1, 1, 1, 2, 3] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
+    }),
+    
+    test('should return true if receive the array [0, -2, 5, 6]', ()=>{
+        const input = [0, -2, 5, 6] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
+    }),
+    
+    test('should return false if receive [1, 2, 3, 4, 5, 3, 5, 6]', ()=>{
+        const input = [1, 2, 3, 4, 5, 3, 5, 6] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
+    }),
+    
+    test('should return false if receive [40, 50, 60, 10, 20, 30]', ()=>{
+        const input = [40, 50, 60, 10, 20, 30] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
+    }),
+    
+    test('should return true if receive [1, 1]', ()=>{
+        const input = [1, 1] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
+    }),
+    
+    test('should return true if receive [1, 2, 5, 3, 5]', ()=>{
+        const input = [1, 2, 5, 3, 5] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
     }),
 
-    test('should return true if receive the array ${test_7}', ()=>{
-        expect(almostIncreasingSequence(test_7)).toBe(true)
+    test('should return false if receive [1, 2, 5, 5, 5]', ()=>{
+        const input = [1, 2, 5, 5, 5] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
+    })
+
+    test('should return false if receive [10, 1, 2, 3, 4, 5, 6, 1]', ()=>{
+        const input = [10, 1, 2, 3, 4, 5, 6, 1] // false
+        expect(almostIncreasingSequence(input)).toBe(false)
     }),
 
-    test('should return false if receive the array ${test_8}', ()=>{
-        expect(almostIncreasingSequence(test_8)).toBe(false)
+    test('should return true if receive [1, 2, 3, 4, 3, 6]', ()=>{
+        const input = [1, 2, 3, 4, 3, 6] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
     }),
 
-    test('should return true if receive the array ${test_9}', ()=>{
-        expect(almostIncreasingSequence(test_9)).toBe(true)
+    test('should return true if receive [1, 2, 3, 4, 99, 5, 6]', ()=>{
+        const input = [1, 2, 3, 4, 99, 5, 6] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
     }),
 
-    test('should return false if receive ${test_10}', ()=>{
-        expect(almostIncreasingSequence(test_10)).toBe(false)
+    test('should return true if receive [123, -17, -5, 1, 2, 3, 12, 43, 45]', ()=>{
+        const input = [123, -17, -5, 1, 2, 3, 12, 43, 45] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
     }),
 
-    test('should return false if receive ${test_11}', ()=>{
-        expect(almostIncreasingSequence(test_11)).toBe(false)
-    }),
-
-    test('should return true if receive ${test_12}', ()=>{
-        expect(almostIncreasingSequence(test_12)).toBe(true)
-    }),
-
-    test('should return true if receive ${test_13}', ()=>{
-        expect(almostIncreasingSequence(test_13)).toBe(true)
+    test('should return true if receive [3, 5, 67, 98, 3]', ()=>{
+        const input = [3, 5, 67, 98, 3] // true
+        expect(almostIncreasingSequence(input)).toBe(true)
     })
 })
